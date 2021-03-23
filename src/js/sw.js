5429527c257667ce2f234e56d3723fd6fbe5b05f
dylantzx <dyla>
2021-03-23 15:15:50 +0800

    organized into folders

--- src/js/sw.js
+++ src/js/sw.js
@@ -3,17 +3,16 @@ self.addEventListener('install', function(event) {
     caches.open('first-app')
       .then(function(cache) {
         cache.addAll([
-          './',
-          './index.html',
-          './info.html',
-          './home.html',
-          './directory.html',
-          './announcements.html',
-          './trends.html',
-          './src/css/style.css',
-          './src/css/index.css',
-          './src/js/app.js',
-          './src/resources/SingHealth_Logo.png',
+          '../html/index.html',
+          '../html/info.html',
+          '../html/home.html',
+          '../html/directory.html',
+          '../html/announcements.html',
+          '../html/trends.html',
+          '../css/style.css',
+          '../css/index.css',
+          '../js/app.js',
+          '../resources/SingHealth_Logo.png',
           'https://kit.fontawesome.com/215800797e.js',
         ])
       })

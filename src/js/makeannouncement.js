1e225ebfdcdd983560419082c5d311e53885a5f0
JibrilTheUltimateFlugel <73127262+JibrilTheUltimateFlugel@users.noreply.github.com>
2021-03-25 18:59:05 +0800

    Edited functons in javascript

--- src/js/makeannouncement.js
+++ src/js/makeannouncement.js
@@ -1,3 +1,8 @@
+const announcementForm = document.querySelector("#announcementForm");
+const title;
+const image;
+const description;
+
 function mock() {
     db.collection("annoucements").doc("new").set({
         title: "New announcement",
@@ -10,4 +15,26 @@ function mock() {
     .catch((error) => {
         console.error("Error writing document: ", error);
     });
-    }
+}
+
+function postAnnouncement(){
+    const title = announcementForm[`titlefield`].value;
+    const image = announcementForm[`imagefield`].value;
+    const description = announcementForm['descriptionfield'].value;
+    console.log(title,image,description);//debugging
+    auth.createUserWithEmailAndPassword(email,password).then(cred => {
+        return db.collection("auditors").doc(cred.user.uid).set({
+            email: email,
+            name: name,
+            hospital: hospital
+        });
+    }).then(()=>{
+        signupForm.reset();
+        window.location.href = "src/html/home.html";
+        signUpError.innerHTML="";
+    }).catch(err =>{
+        console.log(err);
+        signUpError.innerHTML= err.message;
+    });
+};
+
--- src/js/viewannouncement.js
+++ src/js/viewannouncement.js
@@ -4,21 +4,7 @@ const image;
 const description;
 
 function displayInformation(details){
-    const name = `
-        Welcome,<br><span>${details.name}</span>
+    const title= `
+        The title <br><span>${details.title}</span>
     `;
-
-    const html = `
-        <h2><b>Hospital</b><br></h3> 
-        <p>${details.hospital}<br></p>
-        <br>
-
-        <h2><b>Email</b><br></h3> 
-        <p>${details.email}<br></p>
-    `;
-    if (auditorName && accountDetails){
-        auditorName.innerHTML = name;
-        accountDetails.innerHTML = html;
-    }
-    
 }

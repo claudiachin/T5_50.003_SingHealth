1a20080910dff3df06751f4b456115bd4f6d5f8d
JibrilTheUltimateFlugel <73127262+JibrilTheUltimateFlugel@users.noreply.github.com>
2021-03-25 21:31:16 +0800

    Update Announcements

--- src/js/viewannouncement.js
+++ src/js/viewannouncement.js
@@ -3,26 +3,41 @@ const title;
 const image;
 const description;
 
-//retrieve values
-function displayInformation(details){
-    const title= `
-        The title <br><span>${details.title}</span>
-    `;
-
-    return db.collection("auditors").doc(cred.user.uid).set({
-        email: email,
-        name: name,
-        hospital: hospital
-    });
+function retrieveData(){
+db.collection("auditors").onSnapshot(snapshot => {
+    setupDetails(snapshot.docs, user.uid);
+}), err => {
+console.log(err.message);
+}
 }
 
-function bypass(){
-    // get user info
-    const title = "Some Title Here";
-    const description = "This is a test text this is spartan";
-
-    auth.signInWithEmailAndPassword(email, password).then(cred =>{
-        // console.log(cred.user);
-        window.location.href = "src/html/home.html";
+// setup guides
+const setupDetails = (data, id) => {
+    data.forEach(doc => {
+        if (doc.id == id){
+            // console.log(doc.data());
+            displayDetails(doc.data());
+        }
+        
     });
 };
+
+function displayDetails(details){
+    const name = `
+        Welcome,<br><span>${details.name}</span>
+    `;
+
+    const html = `
+        <h2><b>Hospital</b><br></h3> 
+        <p>${details.hospital}<br></p>
+        <br>
+
+        <h2><b>Email</b><br></h3> 
+        <p>${details.email}<br></p>
+    `;
+    if (auditorName && accountDetails){
+        auditorName.innerHTML = name;
+        accountDetails.innerHTML = html;
+    }
+    
+}

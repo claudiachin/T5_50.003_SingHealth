3f1a40978a296f8dede128358959bead765079e8
claudiachin <claudia.chinsh@gmail.com>
2021-04-08 03:02:54 +0800

    update sw.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 // add the serviceWorker
-window.addEventListener('load', function () {
-    navigator.serviceWorker.register('sw.js').then(function (registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function (err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('../../sw.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-});
+}
--- sw.js
+++ sw.js
@@ -1,44 +1,48 @@
+urlsToCache = [
+  './',
+  './index.html',
+  './src/html/info.html',
+  './src/html/home.html',
+  './src/html/directory.html',
+  './src/html/announcements.html',
+  './src/html/announcement_view.html',
+  './src/html/announcement_editor.html',
+  './src/html/pastreports.html',
+  './src/html/trends.html',
+
+  './src/css/style.css',
+  './src/css/index.css',
+  './src/css/bell.css',
+  './src/css/file-document.css',
+  './src/css/create_report.css',
+
+  './src/js/app.js',
+  './src/js/create_report_start.js',
+  './src/js/create_report.js',
+  './src/js/makechart.js',
+  './src/js/index.js',
+  './src/js/multiselect.js',
+
+  './src/resources/SingHealth_Logo.png',
+  '/src/resources/AddPicture.JPG',
+  './src/resources/AddPictureOrange.jpg',
+  './src/resources/profile.png',
+  'src/resources/singhealth_icon_144.png',
+  'src/resources/singhealth_icon_192.png',
+  'src/resources/singhealth_icon_512.png',
+  './src/resources/undraw_Working_oh83.svg',
+
+  'https://kit.fontawesome.com/215800797e.js',
+]
+
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open('first-app')
+    caches.open('singhealth')
       .then(function (cache) {
-        cache.addAll([
-          './',
-          './index.html',
-          './src/html/info.html',
-          './src/html/home.html',
-          './src/html/directory.html',
-          './src/html/announcements.html',
-          './src/html/announcement_view.html',
-          './src/html/announcement_editor.html',
-          './src/html/pastreports.html',
-          './src/html/trends.html',
-          './create_report/start.html',
-
-          './src/css/style.css',
-          './src/css/index.css',
-          './src/css/bell.css',
-          './src/css/file-document.css',
-          './src/css/create_report.css',
-
-          './src/js/app.js',
-          './src/js/create_report_start.js',
-          './src/js/create_report.js',
-          './src/js/makechart.js',
-          './src/js/index.js',
-          './src/js/multiselect.js',
-
-          './src/resources/SingHealth_Logo.png',
-          '/src/resources/AddPicture.JPG',
-          './src/resources/AddPictureOrange.jpg',
-          './src/resources/profile.png',
-          './src/resources/SingHealth_Logo.png',
-          './src/resources/undraw_Working_oh83.svg',
-          'https://kit.fontawesome.com/215800797e.js',
-        ])
+        return cache.addAll(urlsToCache);
       })
   );
-  return self.clients.claim();
+  //return self.clients.claim();
 });
 
 self.addEventListener('fetch', function (event) {
@@ -57,7 +61,7 @@ self.addEventListener('fetch', function (event) {
 
           var responseToCache = response.clone();
 
-          caches.open('first-app')
+          caches.open('singhealth')
             .then(function (cache) {
               cache.put(event.request, responseToCache);
             });

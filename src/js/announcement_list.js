710e5558982c9a5b9397505683c3a149a592d81b
JibrilTheUltimateFlugel <73127262+JibrilTheUltimateFlugel@users.noreply.github.com>
2021-04-08 11:26:46 +0800

    Updated announcement layout

---
+++ src/js/announcement_list.js
@@ -0,0 +1,65 @@
+
+// to change to reading from firebase
+var names = ["New Tenancy Info", "New Rules and Regulations", "Happy Easter to All!", "Merry Christmas dear All", "Rules and Regulations V.1.0", "Chinese New Year Event"];
+
+for (i = 0; i < names.length; i++) {
+    var tenant = document.createElement("p");
+    var tenantText = document.createTextNode(names[i]);
+    tenant.appendChild(tenantText);
+    tenant.classList.add("tenant-text");
+
+    var inst = document.createElement("p");
+    var instText = document.createTextNode("[SingHealth Instiution] [F&B Store]");
+    inst.appendChild(instText);
+
+    var date = document.createElement("h6");
+    var dateText = document.createTextNode("Latest report made on 14/2/2020");
+    date.appendChild(dateText);
+
+    var sect = document.createElement("div");
+    sect.appendChild(tenant);
+    sect.appendChild(inst);
+    sect.appendChild(date);
+    sect.classList.add("sect")
+
+    var icon = document.createElement("div");
+    icon.innerHTML = '<i class="fas fa-chevron-right"></i>'
+
+    var card = document.createElement("div");
+    card.appendChild(sect)
+    card.appendChild(icon);
+    card.classList.add("card");
+    card.id = "card-" + i;
+    card.onclick = function () { selectTenant(this) };
+
+    var split = document.createElement("hr");
+
+    var list = document.getElementById("list");
+    list.appendChild(card);
+    list.appendChild(split);
+}
+
+function filter() {
+    // Declare variables
+    var filter = document.getElementById('search').value.toUpperCase();
+    var cards = document.getElementById("list").getElementsByClassName('card');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < cards.length; i++) {
+        var tenantName = cards[i].firstChild.firstChild;
+        var txtValue = tenantName.textContent || tenantName.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+            cards[i].nextElementSibling.style.display = "";
+        } else {
+            cards[i].style.display = "none";
+            cards[i].nextElementSibling.style.display = "none";
+        }
+    }
+}
+
+function selectAnnouncement(ele) {
+    console.log(ele.id);
+    url = 'tenant_info.html?name=' + encodeURIComponent(ele.firstChild.firstChild.innerHTML);
+    window.location.href = url;
+}

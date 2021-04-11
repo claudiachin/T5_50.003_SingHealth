db6a13b2dc233e74b9e0ea1184e2aa160a4f84a3
claudiachin <claudia.chinsh@gmail.com>
2021-04-12 02:30:03 +0800

    test new service-worker.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 // add the serviceWorker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/T5_50.003_SingHealth/sw.js').then(function (registration) {
+        navigator.serviceWorker.register('/T5_50.003_SingHealth/service-worker.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function (err) {
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "src/",
+  "globPatterns": [
+    "**/*.{css,html,svg,js,JPG,jpg,png}"
+  ],
+  "swDest": "service-worker.js",
+};

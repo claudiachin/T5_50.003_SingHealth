791cc233535d1463088ea8b5b8d240244c7e8d2f
JibrilTheUltimateFlugel <73127262+JibrilTheUltimateFlugel@users.noreply.github.com>
2021-04-21 00:26:32 +0800

    refurbish unit tests

--- src/js/annUnitTest.test.js
+++ src/js/annUnitTest.test.js
@@ -1,20 +1,23 @@
-const { generateText } = require('./makeannouncement');
+const { titleFieldLength } = require('./makeannouncement');
+const { descFieldLength } = require('./makeannouncement');
+const { imageType } = require('./makeannouncement');
 
 document.getElementById("announcement_title").innerHTML = "This is a test to see whether the title bar field value thing can impose the condition of the maximum text length of the 120 characters overtly long abused can";
 document.getElementById("announcement_desc").innerHTML = "Insert 2000+ char string";
 document.getElementById("announcement_image").src = "Image Link";
 
 testMaxTitle('Testing the No of Chars for the Title Field', ()=>{
-    const textLength = document.getElementById("count_value_title").value;
-    expect(len(textLength)).toBeLessThanOrEqual(120); //the number of characters in the textbox must not exceed the maximum allowed chars
+    const textLength = titleFieldLength(); //utilize the titleFieldLength function to return no of chars
+    expect(textLength).toBeLessThanOrEqual(120); //the number of characters in the textbox must not exceed the maximum allowed chars
 });
 
 testMaxDescription('Testing the No of Chars for the Description Field', ()=>{
-    const descLength = document.getElementById("count_value").value;
-    expect(len(descLength)).toBeLessThanOrEqual(2000); //the number of characters in the textbox must not exceed the maximum allowed chars
+    const descLength = descFieldLength;
+    expect(descLength).toBeLessThanOrEqual(2000); //the number of characters in the textbox must not exceed the maximum allowed chars
 });
 
 testImageExtension('Testing the Image Extension uploaded via Upload Image Function',()=>{
     const imageType = document.getElementById("announcement_image").src
     expect(imageType).toBe('.jpg'|'.png'|'.gif'|'.bmp');
 }
+

f60757da6c3aa184959f8b970acdf0f695aa57da
kuro-tora-ryuu <56596164+kuro-tora-ryuu@users.noreply.github.com>
2021-04-21 19:45:08 +0800

    Update new_create_report_start.js

--- src/js/new_create_report_start.js
+++ src/js/new_create_report_start.js
@@ -1,6 +1,37 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+
+
+function newtenant(){
+    //document.getElementById("newformtenant").submit();
+    var hospital = document.getElementById("hospital").value;
+    var branch = document.getElementById("branch").value;
+    var nname = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var newtype = document.getElementById("newtype").value;
+    document.getElementById("nexxt").style.display = 'none';
+// Add a new document with a generated id.
+db.collection("tenants").add({
+    hospital: hospital,
+    branch: branch,
+    name: nname,
+    email: email,
+    type: newtype
+})
+.then((docRef) => {
+    console.log("Tenant document written with ID: ", docRef.id);
+    sessionStorage.setItem("tenantID", docRef.id);
+    sessionStorage.setItem("type", newtype);
+    console.log(newtype);
+})
+.catch((error) => {
+    console.error("Error adding document: ", error);
+});
+
+
+}
+
 var firebaseConfig = {
     apiKey: "AIzaSyBl1hU_vW6IbzkF0XTqvnBlWyLrTmgybns",
     authDomain: "singhealth-221e6.firebaseapp.com",
@@ -20,33 +51,7 @@ var firebaseConfig = {
 
   function goNextnew() {
 
-     //document.getElementById("newformtenant").submit();
-     var hospital = document.getElementById("hospital").value;
-     var branch = document.getElementById("branch").value;
-     var nname = document.getElementById("name").value;
-     var email = document.getElementById("email").value;
-     var newtype = document.getElementById("newtype").value;
- // Add a new document with a generated id.
- db.collection("tenants").add({
-     hospital: hospital,
-     branch: branch,
-     name: nname,
-     email: email,
-     type: newtype
- })
- .then((docRef) => {
-   //var id=docRef.id;
-     console.log("Tenant document written with ID: ", docRef.id);
-     sessionStorage.setItem("tenantID", docRef.id);
-     //console.log(sessionStorage.getItem("tenantID"));
-     sessionStorage.setItem("type", newtype);
-     console.log(newtype);
- })
- .catch((error) => {
-     console.error("Error adding document: ", error);
- });
- document.getElementById("nexxt").style.display = 'none';
- 
+    newtenant();
     db.collection("reports").add({
       associatedTenant: sessionStorage.getItem("tenantID"),
       associatedAuditor: sessionStorage.getItem("auditorID"),
@@ -73,12 +78,11 @@ var firebaseConfig = {
   if (sessionStorage.getItem("reportID") != null) {
     reportID = sessionStorage.getItem("reportID");
     sessionStorage.removeItem("reportID");
-    sessionStorage.removeItem("reportID");
     db.collection("reports").doc(reportID).delete().then(() => {
       console.log("Document successfully deleted!");
     }).catch((error) => {
       console.error("Error removing document: ", error);
     });
 
-   
+ 
   }

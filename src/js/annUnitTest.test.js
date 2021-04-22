4b5ae2ad3738f14bcc4e939dca39261fcfaa9423
JibrilTheUltimateFlugel <73127262+JibrilTheUltimateFlugel@users.noreply.github.com>
2021-04-22 19:55:29 +0800

    standardized test names

--- src/js/annUnitTest.test.js
+++ src/js/annUnitTest.test.js
@@ -1,32 +1,37 @@
-const { titleFieldLength } = require('./makeannouncement');
-const { descFieldLength } = require('./makeannouncement');
-const { imageType } = require('./makeannouncement');
+const titleFieldLength = require('./makeannouncement');
+const descFieldLength = require('./makeannouncement');
+const imageType = require('./makeannouncement');
 
 /*document.getElementById("announcement_title").innerHTML = "This is a test to see whether the title bar field value thing can impose the condition of the maximum text length of the 120 characters overtly long abused can";
 document.getElementById("announcement_desc").innerHTML = "Insert 2000+ char string";
 document.getElementById("announcement_image").src = "Image Link";*/
 
-testMaxTitle('Testing the Max No of Chars for the Title Field', ()=>{
+//testMaxTitle
+test('Testing the Max No of Chars for the Title Field', ()=>{
     const textLength = titleFieldLength(); //utilize the titleFieldLength function to return no of chars
     expect(textLength).toBeLessThanOrEqual(120); //the number of characters in the textbox must not exceed the maximum allowed chars
 });
 
-testMaxDescription('Testing the Max No of Chars for the Description Field', ()=>{
+//testMaxDescription
+test('Testing the Max No of Chars for the Description Field', ()=>{
     const descLength = descFieldLength();
     expect(descLength).toBeLessThanOrEqual(2000); //the number of characters in the textbox must not exceed the maximum allowed chars
 });
 
-testMinTitle('Testing the Min No of Chars for the Title Field', ()=>{
+//testMinTitle
+test('Testing the Min No of Chars for the Title Field', ()=>{
     const textLength = titleFieldLength();
     expect(textLength).toBeGreaterThanOrEqual(0);
 });
 
-testMinDescription('Testing the Min No of Chars for the Description Field', ()=>{
+//testMinDescription
+test('Testing the Min No of Chars for the Description Field', ()=>{
     const descLength = descFieldLength();
     expect(descLength).toBeGreaterThanOrEqual(0);
 });
 
-testImageExtension('Testing the Image Extension uploaded via Upload Image Function',()=>{
+//testImageExtension
+test('Testing the Image Extension uploaded via Upload Image Function',()=>{
     const imageExtension = imageType();
     expect(imageExtension).toBe('jpg'|'png'|'gif'|'bmp');
 });
